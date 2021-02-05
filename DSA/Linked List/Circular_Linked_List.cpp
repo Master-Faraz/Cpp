50a19eb4a01a919c0dc8cb5fb13fa185beeb8cf7
Faraz Ali Ahmad <farazali9028@gmail.com>
2021-02-05 16:18:51 +0530

    ,,

--- DSA/Linked List/Circular_Linked_List.cpp
+++ DSA/Linked List/Circular_Linked_List.cpp
@@ -1,41 +1,40 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct Node 
+struct Node
 {
     int data;
     Node *next;
-}*head;
+} * head;
 
-void create(int A[],int n);
+void create(int A[], int n);
 
 void display(struct Node *p);
 
 int main()
 {
-  int A[]={1,2,3,4,5};
-  create(A,5);
-  display(head);
-  return 0;
+    int A[] = {1, 2, 3, 4, 5};
+    create(A, 5);
+    display(head);
+    return 0;
 }
 
-
-void create(int A[],int n)
+void create(int A[], int n)
 {
-    struct Node *t,*last;
+    struct Node *t, *last;
 
-    head =new Node;
-    head->data=A[0];
-    head->next=head;         // Circular Linked List
-    last=head;
+    head = new Node;
+    head->data = A[0];
+    head->next = head; // Circular Linked List
+    last = head;
 
-    for(int i=1;i<n;i++)
+    for (int i = 1; i < n; i++)
     {
-        Node *t=new Node;
-        t->data=A[i];
-        t->next=last->next;
-        last->next=t;
-        last=t;
+        Node *t = new Node;
+        t->data = A[i];
+        t->next = last->next;
+        last->next = t;
+        last = t;
     }
 }
 
@@ -43,13 +42,12 @@ void display(struct Node *p)
 {
     do
     {
-        cout<<p->data<<endl;
-        p=p->next;
-    }while(p!=head);
+        cout << p->data << endl;
+        p = p->next;
+    } while (p != head);
 }
 
-void insert(struct Node *p,int key,int index)
+void insert(struct Node *p, int key, int index)
 {
-    Node *q=new Node;
-    
+    Node *q = new Node;
 }
