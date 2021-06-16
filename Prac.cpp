16726241c281eeb8e0f0adcd1a6cd0382b9959be
Master-Faraz <farazali9028@gmail.com>
2021-06-16 17:54:55 +0530

    cpp

--- Prac.cpp
+++ Prac.cpp
@@ -1,24 +1,33 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+// int main()
+// {
+//   vector<int> v;
+//   v.push_back(10);
+//   v.push_back(20);
+//   v.push_back(30);
+//   v.push_back(40);
+//   v.push_back(50);
+
+//   vector<int>::iterator itr;
+//   for (itr = v.begin(); itr < v.end(); itr++)
+//     cout << *itr << endl;
+
+//   cout << "\nWithout defining iterator \n"
+//        << endl;
+
+//   for (auto it = v.begin(); it != v.end(); it++)
+//     cout << *it << endl;
+
+//   return 0;
+// }
+
 #include <iostream>
-#include <vector>
 using namespace std;
 int main()
 {
-  vector<int> v;
-  v.push_back(10);
-  v.push_back(20);
-  v.push_back(30);
-  v.push_back(40);
-  v.push_back(50);
-
-  vector<int>::iterator itr;
-  for (itr = v.begin(); itr < v.end(); itr++)
-    cout << *itr << endl;
-
-  cout << "\nWithout defining iterator \n"
-       << endl;
-
-  for (auto it = v.begin(); it != v.end(); it++)
-    cout << *it << endl;
-
+  cout << ((0.1 + 0.2) == 0.3) << endl;
+  cout << (1==1) << endl;
   return 0;
 }
