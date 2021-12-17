cbbea26cec4a24fa6f208d5ee430b6dc259edc1b
Master-Faraz <farazali9028@gmail.com>
2021-12-17 19:58:35 +0530

    c++

--- Prac.cpp
+++ Prac.cpp
@@ -1,26 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-int sum(int n)
-{
-  if(n==0)
-    return 1;
-  else
-    return (n+sum(n-1));
-}
-
-int power(int a,int b)
-{
-  if(b==1)
-    return a;
-  else
-    return (a*(power(a,b-1)));
-}
-
 int main()
 {
-  int n=10;
-  // cout<<sum(n)<<endl;
-  cout<<power(2,0)<<endl;
+  long long int t;
+
+  cin >> t;
+
+  while (t--)
+  {
+    long long int d, D, P, Q;
+
+    cin >> D >> d >> P >> Q;
+
+  
+
+  }
+
   return 0;
 }
